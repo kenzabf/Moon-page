625c77d5a6b06cf5c965201f07045d2c62533190
kenzabf <boufarakenza@gmail.com>
2023-08-12 04:37:44 +0200

    commit 3

---
+++ c/main T.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int age;
+	
+	printf("entre l'age :");
+	scanf("%d", &age);
+		while(age < 10 || age > 10 ){
+		if (age < 10 )(
+		
+		printf("plus petite");
+		)
+	   	
+	   	else (
+		   printf("plus grand");
+		   )
+		   
+		   
+	
+	}
+	return 0;
+}
